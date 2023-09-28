8e8ba4691467cc9f331b7412a093a1af4e8c1f8e
Fralch <ingfralch@gmail.com>
2023-09-28 11:20:20 -0500

    creando animacion automatica

--- components/darpermiso.jsx
+++ components/darpermiso.jsx
@@ -1,13 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import Animated, {
     useSharedValue,
     withTiming,
     useAnimatedStyle,
     Easing,
   } from 'react-native-reanimated';
-  import { View, Button, StyleSheet } from 'react-native';
+  import { Camera, CameraType } from 'expo-camera';
+  import { Button, StyleSheet, Text, TouchableOpacity, View, Dimensions } from 'react-native';
   
+  const { width, height } = Dimensions.get('window');
+
   export default function PermisoCamara() {
+    const [type, setType] = useState(CameraType.back);
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+
     const randomWidth = useSharedValue(10);
+
+    // modificar randomWidth para que sea un valor entre 0 y 350 automaticamente cada 2 segundos
+    useEffect(() => {
+        const interval = setInterval(() => {
+            randomWidth.value = Math.random() * 350;
+        }
+        , 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+
   
     const config = {
       duration: 500,
@@ -17,6 +35,7 @@ import Animated, {
     const style = useAnimatedStyle(() => {
       return {
         width: withTiming(randomWidth.value, config),
+        height: withTiming(randomWidth.value, config),
       };
     });
   
@@ -36,14 +55,22 @@ import Animated, {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'center',
       justifyContent: 'center',
-      flexDirection: 'column',
+      backgroundColor: '#000',
+    },
+    // crear boton circular
+    boton:{
+        backgroundColor: 'red',
+        padding: 20,
+        margin: 20,
+        width: height / 6,
+        height: height / 6,
+        borderRadius: (height / 6) / 2,
     },
     box: {
       width: 100,
       height: 80,
-      backgroundColor: 'black',
+      backgroundColor: 'red',
       margin: 30,
     },
   });
